883e77ee594d9fe1eae4dc0291e7d5ef06aaf9ac
Catirau Mihail <ustmaestro@users.noreply.github.com>
2015-04-05 14:05:08 +0300

    initial comit

---
+++ MobileDetect.php
@@ -0,0 +1,26 @@
+<?php
+namespace app\components;
+ 
+ 
+use Yii;
+use yii\base\Component;
+use yii\base\InvalidConfigException;
+
+$basePath = dirname(__FILE__);
+require_once($basePath . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . 'Mobile_Detect.php');
+ 
+class MobileDetect extends Component
+{
+	private $_mobileDetect;
+	
+    public function init()
+    {
+        $this->_mobileDetect = new Mobile_Detect();
+    }
+	
+    public function __call($name, $parameters)
+    {
+        return call_user_func_array(array($this->_mobileDetect, $name), $parameters);
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# yii2-mobile-detect
+### MobileDetect Yii2 Extension
+> This Extension based on [Mobile_Detect class](https://github.com/serbanghita/Mobile-Detect) writed by Serban Ghita and Victor Stanciu
+
+### Install
+Add your component inside the config/web.php file eg:
+
+```php
+'components' => [
+    ...
+
+    'mobileDetect' => [
+        'class' => 'app\components\MobileDetect'
+   ],
+    
+    ...
+];
+```
+
+### Usage
+```php
+  $detect = Yii::$app->mobileDetect;
+  // call methods
+  $detect->isMobile();
+  $detect->isTablet();
+  $detect->isIphone();
+  ...
+```
+
